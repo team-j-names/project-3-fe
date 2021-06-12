4553b64903241ea768c440e47e30fbc45bc27537
jimogg <jnogana@gmail.com>
2021-06-12 09:54:30 -0400

    Attached Form component for testing.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import ProjectGalleryPage from './Components/ProjectGalleryPage';
 import ProjectPage from './Components/ProjectPage';
+import Form from './Components/Form';
 
 function App() {
   return (
     <div>
-
+      {/* testing form */}
+      <Form />
+      {/* end testing form */}
       <Link to="/projects">
         <h1>HOMEPAGE</h1>
       </Link>
 
-      <Route path="/projects" exact render={() => 
-        <ProjectGalleryPage/>}/>
+      <Route path="/projects" exact render={() =>
+        <ProjectGalleryPage />} />
 
-      <Route path="/projects/:id" render={(routerProps) => 
-        <ProjectPage match={routerProps.match}/>}/>
+      <Route path="/projects/:id" render={(routerProps) =>
+        <ProjectPage match={routerProps.match} />} />
     </div>
   );
 }
