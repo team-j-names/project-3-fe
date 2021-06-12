773bbe2ae8267557a88f50c9af301ca675cf1156
jimogg <jnogana@gmail.com>
2021-06-12 07:23:01 -0400

    Mored pseudo code.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,8 +8,10 @@ import React from 'react';
 // FORM
 // for submissions, user fills form and submits (http: POST )
 // proj details page will have edit buttons, which will include options to update or delete project.  
-// for updates, form is pre-populated  (useState) with fetched data (useEffect?), user edits and submits (http: POST.. or PATCH)
-// for delete, prompt if sure, then send (http:DELETE) request with _id. 
+// for updates, form is populated (useState) with fetched data (useEffect?), user edits and submits (http: POST.. or PATCH)
+// after edit submitted return to details page of proj
+// for delete, prompt if sure, then send (http:DELETE) request with _id.
+// after delete, return to homepage (view all projects)
 // images will be uploaded to AWS, url link will be stored in db with proj data
 
 // DO
