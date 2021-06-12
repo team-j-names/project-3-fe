f664fbcc7d7cfc4fe817920d1ce433d952a0ed91
jimogg <jnogana@gmail.com>
2021-06-12 06:59:21 -0400

    Created Form component.

---
+++ src/Components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Form = () => {
+
+    const [formData, setFormData] = useState()
+    // for submissions, user fills form and submits (http: POST )
+    // for updates, form is populated with fetched data, user edits and submits (http: POST.. or PATCH)
+    // for delete, proj details page will have edit button, which will include delete option.  
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Form;
