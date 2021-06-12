95155c66eba9a4fe03978ad2e7cd36df7dd66875
jimogg <jnogana@gmail.com>
2021-06-12 07:12:55 -0400

    Form pseudo code.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+// CONSTRAINTS
+// only authenticated user can edit/ submit a project
+// user can only edit/delete the projects attached to their user name
+// limit characters in form fields...?
+//
+// FORM
+// for submissions, user fills form and submits (http: POST )
+// proj details page will have edit buttons, which will include options to update or delete project.  
+// for updates, form is pre-populated  (useState) with fetched data (useEffect?), user edits and submits (http: POST.. or PATCH)
+// for delete, prompt if sure, then send (http:DELETE) request with _id. 
+// images will be uploaded to AWS, url link will be stored in db with proj data
+
+// DO
+// fetch a project's details using its ID
+// map fetched details onto details page
+// create formfields
+// labels
+// buttons
+// prevent default
+
 const Form = () => {
 
     const [formData, setFormData] = useState()
-    // for submissions, user fills form and submits (http: POST )
-    // for updates, form is populated with fetched data, user edits and submits (http: POST.. or PATCH)
-    // for delete, proj details page will have edit button, which will include delete option.  
+
     return (
         <div>
 
