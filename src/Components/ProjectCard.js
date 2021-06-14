1987aeeb14a80613c3e8e645cbfdcff7772c8cea
jimogg <jnogana@gmail.com>
2021-06-14 14:35:19 -0400

    Truncated description on card to 75 words + ...

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const ProjectCard = ({ project }) => {
+    // truncate description showed on card
+    let shortDescr;
+    if (project.description.length > 75) {
+        shortDescr = project.description.slice(0, 75).concat("...")
+    } else shortDescr = project.description;
+
     return (
         <div className="project-card">
             <h3>{project.title}</h3>
             <p>{project.author}</p>
             <div>{project.image}</div>
             <p>{project.technologies}</p>
-            <p>{project.description}</p>
+            <p>{shortDescr}</p>
         </div>
     );
 };
