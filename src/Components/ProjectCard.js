a705cdcfb634b8e0cc1aa25e1549337cd607a048
Jarod Torres <me@jarodtorres.com>
2021-06-16 11:25:44 -0400

    Project Page and Card Updates
    Co-authored-by: jimogg <81941464+jimogg@users.noreply.github.com>

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,22 +1,21 @@
-import React from 'react';
 
+import React from 'react';
 const ProjectCard = ({ project }) => {
     // truncate description showed on card
     let shortDescr;
     if (project.description.length > 100) {
         shortDescr = project.description.slice(0, 100).concat("...")
     } else shortDescr = project.description;
-
     return (
-        <div className="project-card">
-            <h3>{project.title}</h3>
-            <p>{project.author}</p>
+        <div className="project-card" style={{ display: "flex", flexDirection: "column", maxHeight: "250" }}>
+            <h3 style={{ marginTop: "5px", marginBottom: "10px" }} > {project.title}</h3>
+            <p style={{ marginTop: "3x", marginBottom: "5px" }}>{project.author}</p>
             {/* use image url */}
+            <div><img src="https://team-j-names-media.s3.us-east-2.amazonaws.com/projects_images2/student_proj_placeholder.png" alt="" width="300px%" /></div>
             <div className="card-image">{project.image}</div>
-            <p>{project.technologies}</p>
+            {/* <p>{project.technologies}</p> */}
             <p>{shortDescr}</p>
         </div>
     );
 };
-
 export default ProjectCard;
